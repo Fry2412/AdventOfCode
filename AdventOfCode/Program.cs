67725073c89bccc5b43530b7f715923ceab4063b
Dominik Metz <dominik.metz@gmail.com>
2020-12-02 08:03:42 +0100

    Day2 p2

--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -45,24 +45,42 @@ namespace AdventOfCode
         {
             var list = Inputs.dayTwo;
             var pwCorrectCount = 0;
+            var pwWithNewPolicyCorrectCount = 0;
 
             foreach (var pw in list)
             {
                 var parts = pw.Split(':');
                 var policy = parts[0];
-                var password = parts[1];
+                var password = parts[1].Trim();
 
                 var bounds = policy.Split(' ')[0].Split('-');
                 var lett = policy.Split(' ')[1];
 
-                var count = password.ToCharArray().Count(x => x.ToString() == lett);
                 Int32.TryParse(bounds[0], out var minBound);
                 Int32.TryParse(bounds[1], out var maxBound);
+
+                //Old
+                var count = password.ToCharArray().Count(x => x.ToString() == lett);
                 pwCorrectCount += count >= minBound && count <= maxBound ? 1 : 0;
+                //
+
+                // New
+                var chars = password.Trim().ToCharArray();
+                if (minBound - 1 < chars.Length && maxBound - 1 < chars.Length)
+                    if ((chars[minBound - 1].ToString() == lett && chars[maxBound - 1].ToString() != lett)
+                        || (chars[minBound - 1].ToString() != lett && chars[maxBound - 1].ToString() == lett))
+                        pwWithNewPolicyCorrectCount++;
+
+                //
+
 
             }
             Console.WriteLine("Passwords fit the policy: ");
             Console.WriteLine(pwCorrectCount);
+
+
+            Console.WriteLine("Passwords fit the new policy: ");
+            Console.WriteLine(pwWithNewPolicyCorrectCount);
             Console.ReadLine();
         }
     }
